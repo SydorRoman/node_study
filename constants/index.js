1de5f54a13aa9810cbb98ae5a493c3dc9fcfc228
SydorRoman <romansydor1998@gmail.com>
2019-03-31 13:29:23 +0300

    [UPGRADE] constants

--- constants/index.js
+++ constants/index.js
@@ -1,3 +1,11 @@
 module.exports = {
-    USER_NOT_FOUND: 'User was not found'
+    PASS_SALT: 10,
+
+    USER_NOT_FOUND: 'User was not found',
+    PASSWORD_REQUIRED: 'Password is required',
+    EMAIL_REQUIRED: 'Email is required',
+    WRONG_PASSWORD: 'Password is incorrect',
+    USER_NOT_EXIST: 'User doen not exist',
+    USER_EXIST: 'User already exist',
+
 }
